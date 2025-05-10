3c46d00e12b11972a8a4d1faa357b804f6068d4e
PARGUNANBHARATHI <gunanbharathi@gmail.com>
2025-05-10 13:05:24 +0530

    update

--- frontend/react/src/pages/Home.jsx
+++ frontend/react/src/pages/Home.jsx
@@ -1,89 +1,221 @@
-import React, { useState } from 'react';
-import { Link } from "react-router-dom";
-import axios from "axios";
+import React, { useState, useEffect } from 'react'
+import { Link } from "react-router-dom"
+import axios from "axios"
 import { ToastContainer, toast } from "react-toastify";
-import "./Home.css";
+import "./Home.css"
 
 function Home() {
+  const slides = [
+    { url: 'images/slide0.svg' },
+    { url: 'images/slide1.svg' },
+    { url: 'images/slide2.svg' },
+    { url: 'images/slide3.svg' },
+    { url: 'images/slide4.svg' },
+    { url: 'images/slide5.svg' }
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      goToNext();
+    }, 3000);
+    return () => clearInterval(interval);
+  }, [currentIndex]);
+
+
+  const goToPrevious = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+
+    setCurrentIndex(newIndex);
+  };
+
+  const goToNext = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+
+    setCurrentIndex(newIndex);
+  };
+
+  const goToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex);
+  };
+
+
   const [name, setname] = useState("");
   const [email, setemail] = useState("");
 
   const onsubmit = async (e) => {
     e.preventDefault();
+
     if (!name || !email) {
-      return toast.warning("Must provide name and email");
+      return toast.warning("Must required name and email");
     }
 
     try {
       const { data } = await axios.post("https://nft1-backend.onrender.com/data", { name, email });
 
       if (data.error) {
-        toast.error(data.error);
-      } else {
+        return toast.error(data.error);
+      }
+      else {
         setemail("");
         setname("");
-        toast.success("Successfully submitted");
+        toast.success("Successfuly submit");
       }
-    } catch (error) {
-      console.log(error);
-      toast.error("Something went wrong");
-    }
-  };
 
+    }
+
+    catch (error) {
+      console.log(error);
+    }
+
+
+  };
   return (
+
     <div>
       <ToastContainer />
+      <div className="slides">
 
-      {/* Section: Who We Are */}
+        <div className="slider-container">
+          <div className="slider">
+            <div className="slides-container"
+              style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+            >
+              {slides.map((slide, index) => (
+                <div
+                  className="slide"
+                  key={index}
+                  style={{ backgroundImage: `url(${slide.url})` }}
+
+                ></div>
+              ))}
+            </div>
+          </div>
+
+          {/* Left Arrow */}
+          <div className="left-arrow" onClick={goToPrevious}>&#10094;</div>
+
+          {/* Right Arrow */}
+          <div className="right-arrow" onClick={goToNext}>&#10095;</div>
+
+          {/* Dots Indicator */}
+          <div className="dots-container">
+            {slides.map((_slide, slideIndex) => (
+              <div
+                className={`dot ${slideIndex === currentIndex ? 'active' : ''}`}
+                key={slideIndex}
+                onClick={() => goToSlide(slideIndex)}
+              ></div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+
       <div className="container my-5">
-        <div className="row align-items-center">
-          <div className="col-lg-7 mb-0">
-            <h2 className="heading mb-0 ms-1">Who We Are</h2>
-            <div className="intro-text ms-1">
-              <p>
-                Nation's First Trust (NFT) is dedicated to transforming Bharat into a developed nation. We aim to build a foundation of trust by focusing on sustainable growth, community empowerment, and innovation. Our initiatives are designed to enhance social welfare.
-              </p>
-            </div>
-            <div className="vision-text ms-1">
-              <p>
-                Bharat, a land of rich heritage and immense potential, stands at the cusp of a new era. With its vibrant culture, diverse traditions, and strong values, it has long been a guiding light of selfless service and cultural strength. NFT envisions harnessing this strength to build a self-reliant and progressive nation.
-              </p>
-            </div>
-            <div className="mission-text ms-1">
-              <p>
-                NATION'S FIRST TRUST is a registered Charitable Trust. NFT perceives its role as a catalyst in revitalizing cultural sensitivity, bridging tradition and modernity in today’s world.
-              </p>
-            </div>
-            <h5 className="signature mt-0 text-end">- Nation's First Trust</h5>
-          </div>
+  <div className="row align-items-center">
 
-          {/* Image Container (Right Side) */}
-          <div className="col-lg-5 text-center">
-            <div className="custom-image-container">
-              <img
-                src="images/homein.png"
-                alt="Who We Are"
-                className="img-fluid rounded shadow"
-              />
-            </div>
-          </div>
-        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <div className="container my-5">
+  <div className="row align-items-center">
+    {/* Paragraph Container (Left Side) */}
+    <div className="col-lg-7 mb-0 ">
+      <h2 className="heading mb-0 ms-1 ">Who We Are</h2>
+      
+      <div className="intro-text ms-1  ">
+        <p>
+          Nation's First Trust (NFT) is dedicated to transforming Bharat into a developed nation.
+          We aim to build a foundation of trust by focusing on sustainable growth, community empowerment, and innovation.
+          Our initiatives are designed to enhance social welfare.
+        </p>
       </div>
 
-      {/* Section: Join Us */}
+      <div className="vision-text ms-1 ">
+        <p>
+          Bharat, a land of rich heritage and immense potential, stands at the cusp of a new era.
+          With its vibrant culture, diverse traditions, and strong values, it has long been a guiding light of selfless service
+          and cultural strength. NFT envisions harnessing this strength to build a self-reliant and progressive nation.
+        </p>
+      </div>
+
+      <div className="mission-text ms-1">
+        <p>
+          NATION'S FIRST TRUST is a registered Charitable Trust. NFT perceives its role as a catalyst in revitalizing cultural sensitivity,
+          bridging tradition and modernity in today’s world.
+        </p>
+      </div>
+      
+      <h5 className="signature mt-0 text-end">- Nation's First Trust</h5>
+    </div>
+
+    {/* Image Container (Right Side) */}
+    <div className="col-lg-5 text-center">
+  <div className="custom-image-container">
+    <img
+      src="images/homein.png"
+      alt="Who We Are"
+      className="img-fluid rounded shadow"
+    />
+  </div>
+</div>
+  </div>
+</div>
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
       <div className="container1">
-        <div className="d-flex flex-column justify-content-center align-items-center gap-3">
-          <Link className="btn-custom btn-custom-success" to="/Donation">
-            Make a Donation🤍
-          </Link>
-          <Link className="btn-custom btn-custom-primary" to="/Volunteer">
-            Become a Volunteer
-          </Link>
-        </div>
-        <img src='images/homejoin.svg' alt="Join Us" />
-      </div>
 
-      {/* Section: Background Image with Heading */}
+        <div className='join-commu'>
+          {/* <h5>Nation's First Trust</h5>  */}
+          {/* <h4>Get involved in our social welfare and charity<br></br>initiatives</h4> */}
+          {/* <h4>Join our foundation today and start building<br></br> the
+          future of our society.</h4>  */}
+<div className="d-flex flex-column justify-content-center align-items-center gap-3">
+  <Link className="btn-custom btn-custom-success" to="/Donation">
+    Make a Donation🤍
+  </Link>
+  <Link className="btn-custom btn-custom-primary" to="/Volunteer">
+    Become a Volunteer
+  </Link>
+</div>
+
+        </div>
+      
+        <img src='images/homejoin.svg'></img>
+      </div>
       <div className="background-image">
         <div className="backgroun-image-head">
           <h5>Testimonials</h5>
@@ -92,7 +224,6 @@ function Home() {
         </div>
       </div>
 
-      {/* Section: Sign up */}
       <div className="input-head">
         <h6>Join Us</h6>
         <h3>Sign up to hear from us about our new activities.</h3>
@@ -100,25 +231,17 @@ function Home() {
 
       <div className="input-box">
         <form onSubmit={onsubmit} autoComplete='off'>
-          <input
-            className='btn1'
-            type="text"
-            placeholder='Enter Name'
-            value={name}
-            onChange={(e) => setname(e.target.value)}
-          />
-          <input
-            className="btn2"
-            type='email'
-            placeholder='Enter Email'
-            value={email}
-            onChange={(e) => setemail(e.target.value)}
-          />
-          <button type="submit">SUBMIT</button>
+          <input className='btn1' type="text" placeholder='Enter Name' value={name}
+            onChange={((e) => (setname(e.target.value)))} />
+
+          <input className="btn2" type='Email' placeholder='Enter Email' value={email}
+            onChange={((e) => (setemail(e.target.value)))} />
+
+          <button onClick={onsubmit}>SUBMIT</button>
         </form>
       </div>
     </div>
   );
 }
 
-export default Home;
+export default Home
